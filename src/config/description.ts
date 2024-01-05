b8471f1a49c88dd033b4c9d48c35a6e5899897c3
daxchx <daxchx.v1@gmail.com>
2024-01-06 01:49:59 +0900

    Feat: #35 UI調整

---
+++ src/config/description.ts
@@ -0,0 +1,36 @@
+export default class Description {
+  static war = [
+    {
+      title: '基本の遊び方',
+      text: 'カードをたくさんゲットした方の勝ち！',
+      imgSrc: 'assets/games/war/desc1.png'
+    },
+    {
+      title: '勝負',
+      text: '手札から1枚 選んで出して<br>カード同士をバトルさせましょう',
+      imgSrc: 'assets/games/war/desc1.png'
+    },
+    {
+      title: 'そして',
+      text: '相手より強いカードだったら<br>そのカードをゲット！',
+      imgSrc: 'assets/games/war/desc1.png'
+    },
+    {
+      title: 'カードの強さ',
+      text: '「2」が一番弱くて 「A」が一番強い<br>2 → 3 → 4 → … J → Q → K → A',
+      imgSrc: 'assets/games/war/desc1.png'
+    },
+    {
+      title: 'ひきわけ',
+      text: '同じ強さだったら もう一回勝負だ！',
+      imgSrc: 'assets/games/war/desc1.png'
+    },
+    {
+      title: 'チャンス',
+      text: 'ひきわけの後に勝てば<br>カードをまとめてゲット！',
+      imgSrc: 'assets/games/war/desc1.png'
+    }
+  ]
+
+  static blackjack = []
+}
--- src/view/initialView.ts
+++ src/view/initialView.ts
@@ -1,18 +1,23 @@
 export class InitialView {
-  constructor() {
-    console.log('InitialView constructor')
-  }
-
   static create() {
     const root = document.getElementById('app')
 
     const container = document.createElement('div')
 
     container.innerHTML = `
-      <h1>Playing Card Games</h1>
-      <div>
-        <input type="text" id="name-input" placeholder="Enter your name" class='mt-3 border-2 border-black rounded-md p-2'>
-        <button id="start-button">Start</button>
+      <div class="relative w-full h-screen flex flex-col justify-center items-center">
+        <h1 class="text-lg font-bold text-center">Playing Card Games</h1>
+        <div class="w-full p-6">
+          <div class="w-full py-6 px-4 border border-[#2c3239] rounded-lg bg-[#0C1117]">
+            <span class="text-sm text-[#848D97]">Enter your name</span>
+            <div class="mt-2">
+              <input type="text" id="name-input" placeholder="name" class='w-full h-10 px-2 border border-[#2C3239] rounded-md bg-[#15191E]'>
+            </div>
+            <div class="mt-4">
+              <button id="start-button" class="w-full h-10 rounded bg-white font-bold text-[#0C1117]">Start</button>
+            </div>
+          </div>
+        </div>
       </div>
     `
 
