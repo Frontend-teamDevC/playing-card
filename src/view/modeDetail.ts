043216d5ec5d7469685fe530b5ebe3f018b4e9ea
daxchx <daxchx.v1@gmail.com>
2024-01-15 08:41:13 +0900

    #42 Fix:不要なインポートの削除

--- src/view/modeDetail.ts
+++ src/view/modeDetail.ts
@@ -40,29 +40,7 @@ export class ModeDetail {
         </div>
       </div>
     </div>
-    
     `
-
-    new Swiper('.swiper', {
-      loop: true,
-      slidesPerView: 'auto',
-
-      // If we need pagination
-      pagination: {
-        el: '.swiper-pagination'
-      },
-
-      // Navigation arrows
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-      },
-
-      // And if we need scrollbar
-      scrollbar: {
-        el: '.swiper-scrollbar'
-      }
-    })
   }
 
   static regulation(reg: string[]): string {
